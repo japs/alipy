abc44cd429875ced9393e8987e87d7ee7ac1238e
mtewes <mtewes@5ae55fb9-db83-4c1d-9d4d-9b20baeada1b>
2012-09-29 09:00:44 +0000

    doc
    git-svn-id: https://svn.epfl.ch/svn/mtewes-public/trunk/alipy2@36 5ae55fb9-db83-4c1d-9d4d-9b20baeada1b

---
+++ alipy/__init__.py
@@ -0,0 +1,14 @@
+"""
+alipy 
+=====
+
+A module to automatically align -- or just identify geometrical transforms -- between astronomical images.
+"""
+
+__author__ = "Malte Tewes"
+__copyright__ = "2012, Malte Tewes"
+__version__ = "2.0"
+
+import alipy, pysex, star
+
+
