864b90e84087ea4605d98eea8a6381c8492c78f3
Jacopo Nespolo <j.nespolo@gmail.com>
2015-07-14 00:03:26 +0200

    autopep8

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@
 from distutils.core import setup
 
 setup(
-	name='alipy',
-	version='2.0dev',
-	packages=['alipy'],
-	description='alipy',
-	long_description=open('README.txt').read(),
-	author='Malte Tewes',
-	license='GPLv3',
-	author_email='malte.tewes[at]epfl.ch',
-	url='http://obswww.unige.ch/~tewes/alipy/'
+    name='alipy',
+        version='2.0dev',
+        packages=['alipy'],
+        description='alipy',
+        long_description=open('README.txt').read(),
+        author='Malte Tewes',
+        license='GPLv3',
+        author_email='malte.tewes[at]epfl.ch',
+        url='http://obswww.unige.ch/~tewes/alipy/'
 )
-
